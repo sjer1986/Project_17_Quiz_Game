1adf98e85ef4ee3769cc6dd9ee387ec2e18308f0
sjer1986 <88799824+sjer1986@users.noreply.github.com>
2022-02-07 20:50:46 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,101 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "'Typewriter' is the longest word that can be typed using only "
+                    "the first row on a QWERTY keyboard.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The sum of all the numbers on a roulette wheel is 666.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An eggplant is a vegetable.",
+        "correct_answer": "False",
+        "incorrect_answers": "True"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Cucumbers are usually more than 90% water.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The average woman is approximately 5 inches (13 centimeters) "
+                    "shorter than the average man.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Kissing someone for one minute burns about 2 calories.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The French word for 'glass' is 'glace'",
+        "correct_answer": "False",
+        "incorrect_answers": "True"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Albert Einstein had trouble with mathematics when he was in school.",
+        "correct_answer": "False",
+        "incorrect_answers": "True"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Fast food restaurant chains Carl's Jr. and Hardee's are owned by the same company.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "'Santa Claus' is a variety of melon.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Haggis is traditionally eaten on Burns Night.",
+        "correct_answer": "True",
+        "incorrect_answers": "False"
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The pickled gherkin was first added to hamburgers because a US health law \n"
+                    "required all fast-food to include a source of Vitamin C.",
+        "correct_answer": "False",
+        "incorrect_answers": "True"
+    }
+]
